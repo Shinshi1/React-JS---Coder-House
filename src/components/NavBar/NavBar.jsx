7b9a732e0aaf75db3d443cda094fe44d42671993
gabriel hidalgo <gabi_hidalgo1@hotmail.com>
2022-11-01 01:01:37 -0300

    creamos NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <a href="/">
+                <h2>Postres La Creme</h2>
+                </a>
+            </li>
+            <li>
+                <a href="/">Tortas</a>
+            </li>
+            <li>
+                <a href="/">Tartas</a>
+            </li>
+            <li>
+                <a href="/">Postresgit remote add origin https://github.com/Shinshi1/React-JS---Coder-House.git</a>
+            </li>
+            <li>
+                <img src="/assets/cart.svg" alt="Carrito de compras" />
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
